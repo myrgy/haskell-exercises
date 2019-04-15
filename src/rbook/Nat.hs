7871df7424ae4b20f6d6884ba130a6578534c21a
Alexander Dalshov <dalshov@gmail.com>
2019-04-15 22:21:07 +0300

    + Nat.hs

---
+++ src/rbook/Nat.hs
@@ -0,0 +1,16 @@
+module Nat where
+
+data Nat = Zero | Succ Nat
+  deriving (Show, Eq, Ord)
+
+instance Num Nat where
+        negate _ = error "negate is undefined for Num"
+        (+) a Zero     = a
+        (+) a (Succ b) = Succ (a + b)
+        (*) a Zero     = Zero
+        (*) a (Succ b) = a + (a * b)
+        abs x = x
+        signum Zero = Zero
+        signum _    = Succ Zero
+        fromInteger 0 = Zero
+        fromInteger n = Succ (fromInteger (n - 1))
