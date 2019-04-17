8dbe91b6a644fc9be0c82d5cd82347d141919674
Alexander Dalshov <dalshov@gmail.com>
2019-04-17 08:40:35 +0300

    UT experiments

--- test/rbook/NatTests.hs
+++ test/rbook/NatTests.hs
@@ -1,11 +1,13 @@
-import           Test.Hspec
+module NatTests (spec) where
 
-main :: IO ()
-main = hspec $ do
+import  Test.Hspec
+
+spec::Spec
+spec = hspec $ do
         describe "Nat" $ do
                 describe "beside" $ do
                         beside 1 1 `shouldBe` False
-                        beside 3 1 `shouldBe` False
+                        beside 3 1 `shouldBe` True
                         beside 3 2 `shouldBe` True
                         beside 1 5 `shouldBe` False
                         beside 4 5 `shouldBe` True
